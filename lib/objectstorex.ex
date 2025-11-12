262f4f3620f03cc9227d35b579264c5f8f98a4ea
Roman Heinrich <roman.heinrich@gmail.com>
2025-11-12 03:06:32 +0100

    Init

---
+++ lib/objectstorex.ex
@@ -0,0 +1,18 @@
+defmodule Objectstorex do
+  @moduledoc """
+  Documentation for `Objectstorex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Objectstorex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/objectstorex_test.exs
@@ -0,0 +1,8 @@
+defmodule ObjectstorexTest do
+  use ExUnit.Case
+  doctest Objectstorex
+
+  test "greets the world" do
+    assert Objectstorex.hello() == :world
+  end
+end
