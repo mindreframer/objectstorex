a3ba63a375770cf91f45c5b805f965f3ae5cd542
Roman Heinrich <roman.heinrich@gmail.com>
2026-08-02 21:25:35 +0200

    Add v0.2.0 precompiled NIF checksums

---
+++ checksum-Elixir.ObjectStoreX.Native.exs
@@ -0,0 +1,10 @@
+%{
+  "libobjectstorex-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:531efa7edba5545faf4db520a493e9d7a0c4b60e61f6528ec8d0f7b6312b35c7",
+  "libobjectstorex-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:135272109131aeb2b317d622fbc8b0698cdf85098abd24d4ee175192768f2be8",
+  "libobjectstorex-v0.2.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:6deec4fe228f4508a302b0a7b5ad2b3324615e18e5cd8a392b8e1b95a4d21f08",
+  "libobjectstorex-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:324c879668e0c66f4a81e28847fa81f21cddb8b8ad7212e5ba22b236c3d69705",
+  "libobjectstorex-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:e5f3077b825ba69ce62b212647291535db87d7ce50ed23bb5de0306d02d6ff93",
+  "libobjectstorex-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:92764dc725812edc8cf3e5f3042858ececd9f3be66b166a564ac6e0291c092a4",
+  "objectstorex-v0.2.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:2bfd25bf37c036c15f27e40726da9393c345cc58d32d00059825851726966d46",
+  "objectstorex-v0.2.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:8952de1fcb5ae309ff18125faffd754624c8ea608019d8a1a36ff89e56f823b0",
+}
